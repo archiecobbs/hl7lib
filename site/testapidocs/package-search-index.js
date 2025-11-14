81984b69e1f2aff02b651f0ad99434d77b55021b
Archie L. Cobbs <archie.cobbs@gmail.com>
2025-11-14 16:36:13 -0600

    Regenerate Maven site.

--- site/testapidocs/package-search-index.js
+++ site/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.dellroad.hl7"},{"l":"org.dellroad.hl7.io"},{"l":"org.dellroad.hl7.llp"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.dellroad.hl7","l":"org.dellroad.hl7"},{"m":"org.dellroad.hl7","l":"org.dellroad.hl7.io"},{"m":"org.dellroad.hl7","l":"org.dellroad.hl7.llp"}];updateSearchResults();
